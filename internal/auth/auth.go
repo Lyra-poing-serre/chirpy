8efd5d2fc652bfc9ffa82c2e92db1146516120c7
Lyra-poing-serre <23309908+Lyra-poing-serre@users.noreply.github.com>
2025-07-03 14:18:12 +0200

    auth JWT done

---
+++ internal/auth/auth.go
@@ -0,0 +1,53 @@
+package auth
+
+import (
+	"errors"
+	"time"
+
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string) (string, error) {
+	pwd, err := bcrypt.GenerateFromPassword([]byte(password), 0)
+	return string(pwd), err
+}
+
+func CheckPasswordHash(password, hash string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+}
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
+		Issuer:    "chirpy",
+		IssuedAt:  jwt.NewNumericDate(time.Now()),
+		ExpiresAt: jwt.NewNumericDate(time.Now().Add(expiresIn)),
+		Subject:   userID.String(),
+	})
+	signedToken, err := token.SignedString([]byte(tokenSecret))
+	if err != nil {
+		return "", err
+	}
+	return signedToken, nil
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &jwt.RegisteredClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(tokenSecret), nil
+	})
+	if err != nil {
+		return uuid.UUID{}, err
+	}
+
+	claims, ok := token.Claims.(*jwt.RegisteredClaims)
+	if !ok {
+		return uuid.UUID{}, errors.New("unexpected type of claims")
+	}
+
+	uId, err := uuid.Parse(claims.Subject)
+	if err != nil {
+		return uuid.UUID{}, err
+	}
+	return uId, nil
+}
---
+++ internal/auth/auth_test.go
@@ -0,0 +1,55 @@
+package auth
+
+import (
+	"crypto/rand"
+	"testing"
+	"time"
+
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func TestHashPassword(t *testing.T) {
+	rdm := make([]byte, 10)
+	rand.Read(rdm)
+
+	result, err := HashPassword(string(rdm))
+	if err != nil {
+		t.Errorf("got unexpected error with HashPassword: %s", err)
+	}
+
+	if bcrypt.CompareHashAndPassword([]byte(result), rdm) != nil {
+		t.Errorf("the password and its hash do not match")
+	}
+}
+
+func TestCheckPasswordHash(t *testing.T) {
+	rdm := make([]byte, 10)
+	rand.Read(rdm)
+
+	expected, err := bcrypt.GenerateFromPassword(rdm, 0)
+	if err != nil {
+		t.Errorf("got unexpected error with bcrypt.GenerateFromPassword: %s", err)
+	}
+
+	if bcrypt.CompareHashAndPassword(expected, rdm) != CheckPasswordHash(string(rdm), string(expected)) {
+		t.Errorf("hash didn't match")
+	}
+}
+
+func TestMakeAndValidateJWT(t *testing.T) {
+	mockPwd := make([]byte, 10)
+	rand.Read(mockPwd)
+	expected := uuid.New()
+	signedToken, err := MakeJWT(expected, string(mockPwd), 1*time.Hour)
+	if err != nil {
+		t.Error("got unexpected error with MakeJWT")
+	}
+	result, err := ValidateJWT(signedToken, string(mockPwd))
+	if err != nil {
+		t.Errorf("got unexpected error with ValidateJWT: %s", err)
+	}
+	if result != expected {
+		t.Errorf("userId didn't match")
+	}
+}
